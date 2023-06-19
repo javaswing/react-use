a7b653c566786c4dc6113e68e280d035af4a9574
javaswing <zxdlovejava@gmail.com>
2023-06-19 19:39:32 +0800

    chore: init jest typescript

---
+++ src/useTimeout.ts
@@ -0,0 +1,19 @@
+import { useCallback, useEffect, useRef } from "react";
+
+export default function useTimeout(fn: () => void, delay?: number) {
+  const clearFlag = useRef<NodeJS.Timeout | null>(null);
+
+  const clear = useCallback(() => {
+    if (clearFlag.current) {
+      clearTimeout(clearFlag.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof delay === "undefined" || delay < 0) return;
+    clearFlag.current = setTimeout(fn, delay);
+    return clear;
+  }, [fn, delay]);
+
+  return clear;
+}
